095441fe78e35a89825b66c772a1a1cb3531d744
YiLiangXie <microos316@gmail.com>
2017-04-07 02:12:05 +0800

    clean files

--- src/hk/microos/data/Flags.java
+++
@@ -1,8 +0,0 @@
-package hk.microos.data;
-
-public class Flags {
-	public static boolean GLOABAL_DEBUG = true;
-	public static boolean FUNC_INVOKE_PRINT = true;
-	public static double minStroke = 2;
-	public static double maxStroke = 5;
-}
